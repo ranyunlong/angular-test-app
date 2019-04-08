90f2d1b7e59fdc9ea52da38f676802bda6de68a2
ranyunlong <549510622@qq.com>
2019-04-08 10:47:18 +0800

    update

--- src/app/admin/user/user.component.ts
+++ src/app/admin/user/user.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from '@angu
 import { HttpClient } from '@angular/common/http';
 import { BaseHttpResponse } from '../admin.interceptor';
 import { NzNotificationService } from 'ng-zorro-antd';
+import { FormBuilder, FormGroup, Validators, AbstractControl, FormControl } from '@angular/forms';
+import { isMobilePhone } from 'validator';
+import { Validation } from 'src/app/lib/Validation';
 
 @Component({
     selector: 'app-user',
@@ -12,7 +15,9 @@ export class UserComponent implements OnInit, OnChanges {
     private iPage = 1;
     private iLimit = 8;
     private iUsername = '';
-
+    private isModalVisible = false;
+    private modalTitle = '';
+    private OkLoading = false;
     public get page() {
         return this.iPage;
     }
@@ -39,8 +44,33 @@ export class UserComponent implements OnInit, OnChanges {
     public set username(value: string) {
         if (this.iUsername !== value) {
             this.iUsername = value;
+            this.iPage = 1;
             this.getUserList();
         }
+    }
+
+    public get isCheckAll() {
+        return  this.list.filter((item) => {
+            return item.selected;
+         }).length === this.list.length;
+    }
+
+    public set isCheckAll(val: boolean) {
+        this.list = this.list.map((item) => {
+            item.selected = val;
+            return item;
+        });
+    }
+
+    public get checkedList() {
+        return this.list.filter((item) => item.selected);
+    }
+
+    public get isCheck() {
+        const resuslt = this.list.filter((item) => {
+            return item.selected;
+        });
+        return resuslt.length > 0 && resuslt.length !== this.list.length;
     }
 
     public sidx = 'userId';
@@ -48,10 +78,45 @@ export class UserComponent implements OnInit, OnChanges {
     public total = 1;
     public list: Array<{ [K in keyof List]?: List[K] }> = [];
     public loading = false;
+    public editData: List | null = null;
+
+    // 表单组的验证规则
+    private validateForm: FormGroup;
     constructor(
         private http: HttpClient,
-        private notification: NzNotificationService
-    ) { }
+        private notification: NzNotificationService,
+        private formBuilder: FormBuilder
+    ) {
+
+        const formGroup: { [K in keyof UserForm]?: any } = {
+            username: new FormControl(null, [
+                Validation.required('密码必须'),
+                Validation.isLength({min: 5, max: 16}, '账号长度必须5-16位')
+            ]),
+            password: new FormControl(null, [
+                Validation.required('密码必须'),
+                Validation.isLength({min: 5, max: 16}, '密码长度必须5-16位')
+            ]),
+            mobile: new FormControl(null, [
+                Validation.required('手机必须'),
+                    Validation.isMobile('手机号码格式不正确')
+            ]),
+            email: new FormControl(null, [
+                Validation.required('邮箱必须'),
+                Validation.isEmail('邮箱格式不正确')
+            ]),
+            status: new FormControl(true)
+        };
+        this.validateForm = formBuilder.group(formGroup);
+    }
+
+    /**
+     * 获取表单错误
+     * @param key 表单的字段名称
+     */
+    public getFormError(key: string): string {
+        return this.validateForm.get(key).errors.message;
+    }
 
     ngOnInit() {
         this.getUserList();
@@ -61,6 +126,13 @@ export class UserComponent implements OnInit, OnChanges {
         console.log(arg);
     }
 
+    public validate(key: string) {
+        return this.validateForm.get(key).errors && this.validateForm.get(key).dirty;
+    }
+
+    /**
+     * 获取用户列表
+     */
     private getUserList() {
         this.loading = true;
         const { page, limit, sidx, order, username } = this;
@@ -73,7 +145,6 @@ export class UserComponent implements OnInit, OnChanges {
                 username
             }
         }).subscribe((res: UserListResponse) => {
-            console.log(res)
             const { code, msg } = res;
             if (code !== undefined && code === 0 && res.page.list) {
                 this.list = res.page.list;
@@ -87,6 +158,90 @@ export class UserComponent implements OnInit, OnChanges {
             }
             this.loading = false;
         });
+    }
+
+    /**
+     * 处理模态框ok事件
+     */
+    private handleModalOk() {
+        // this.isModalVisible = false;
+        // this.OkLoading = true;
+        // 更新验证
+        for (const item in  this.validateForm.controls) {
+            this.validateForm.controls[item].markAsDirty();
+            this.validateForm.controls[item].updateValueAndValidity();
+        }
+
+        if (this.validateForm.valid) {
+            const userInfo = this.validateForm.value as UserForm;
+            userInfo.status = userInfo.status ? 0 : 1;
+            if (this.modalTitle === '新增管理员') {
+                userInfo.userId = userInfo.userId ? userInfo.userId : 0;
+                userInfo.roleIdList = [0];
+                this.OkLoading = true;
+                return this.http.post('/sys/user/save', userInfo).subscribe((res: BaseHttpResponse) => {
+                    const { code, msg } = res;
+                    if (code === 0) {
+                        this.notification.success('成功', '已添加');
+                        this.OkLoading = false;
+                        this.isModalVisible = false;
+                        this.getUserList();
+                    } else {
+                        this.notification.error('失败', msg);
+                        this.OkLoading = false;
+                    }
+                });
+            } else {
+                this.OkLoading = true;
+                if (this.editData.password.substr(0, 6) === userInfo.password) {
+                    delete this.editData.password;
+                    delete userInfo.password;
+                }
+                return this.http.post('/sys/user/update', {
+                    ...this.editData,
+                    ...userInfo
+                }).subscribe((res: BaseHttpResponse) => {
+                    const { code, msg } = res;
+                    if (code === 0) {
+                        this.notification.success('成功', '已更新');
+                        this.OkLoading = false;
+                        this.isModalVisible = false;
+                        this.getUserList();
+                    } else {
+                        this.notification.error('失败', msg);
+                        this.OkLoading = false;
+                    }
+                });
+            }
+        }
+    }
+
+    handleEdit(data: List) {
+        this.editData = data;
+        // console.log(data)
+        this.isModalVisible = true;
+        this.modalTitle = '修改用户';
+        this.validateForm.setValue({
+            username: data.username,
+            password: data.password.substr(0, 6),
+            mobile: data.mobile,
+            status: data.status === 0 ? true : false,
+            email: data.email
+        });
+
+        // this.validateForm
+    }
+
+    private deleteMany() {
+        console.log(1, this.checkedList);
+    }
+
+    /**
+     * 处理模态框的取消事件
+     */
+    private handleModalCancel() {
+        this.isModalVisible = false;
+        this.validateForm.reset();
     }
 
 }
@@ -104,6 +259,19 @@ interface Page {
 }
 
 interface List {
+    selected?: boolean;
+    userId: number;
+    username: string;
+    password: string;
+    email: string;
+    mobile: string;
+    status: number;
+    roleIdList: number[];
+    createUserId: number;
+    createTime: string;
+}
+
+interface UserForm {
   userId: number;
   username: string;
   password: string;
@@ -111,6 +279,4 @@ interface List {
   mobile: string;
   status: number;
   roleIdList: number[];
-  createUserId: number;
-  createTime: string;
 }
---
+++ src/app/lib/Validation.ts
@@ -0,0 +1,57 @@
+import { isEmail, isEmpty, isMobilePhone, isLength } from 'validator';
+import { AbstractControl } from '@angular/forms';
+
+function formatData(value: any): string {
+    if (typeof value === 'object' && value  !== null) {
+        return JSON.stringify(value);
+    } else {
+        return String(value);
+    }
+}
+
+export class Validation {
+
+    static required(message: string = 'Invalid param is required') {
+        return (control: AbstractControl) => {
+            if (control.value === null) {
+                return { message };
+            }
+            return (isEmpty(formatData(control.value)) ? { message } : null);
+        };
+    }
+
+    static isEmail(message: string = 'Invalid param email.') {
+        return (control: AbstractControl) => {
+            if (!control.value) {
+                return null;
+            }
+            if (!isEmail(String(control.value))) {
+                return { message };
+            }
+        };
+    }
+
+    static isMobile(message: string = 'Invalid param mobile phone.') {
+        return (control: AbstractControl) => {
+            if (!control.value) {
+                return null;
+            }
+            if (!isMobilePhone(String(control.value), 'zh-CN')) {
+                return { message };
+            }
+        };
+    }
+
+    static isLength(options: { min?: number, max?: number } = {}, message: string = 'Invalid param length') {
+        return (control: AbstractControl) => {
+            if (!control.value) {
+                return null;
+            }
+            if (!isLength(formatData(control.value), options)) {
+                return { message };
+            }
+        };
+    }
+};
+
+
